7d708a82f6baa1885396165e945ba5ded9e09728
Josh Shone <josh.shone@gmail.com>
2020-03-21 13:57:18 +0100

    Refactor to use a separate questions.json file + add detail text for each answer

--- main.js
+++ main.js
@@ -1,18 +1,50 @@
-function goToSection(sectionId) {
-  window.history.pushState(null, '', '#' + sectionId);
-  document.documentElement.scrollTo(0, document.getElementById(sectionId).offsetTop);
-}
+(async function() {
+  const response = await fetch('questions.json');
+  const questions = await response.json();
 
-document.body.onclick = event => {
-  if (event.target.classList.contains('back-button')) {
-    goToSection('start');
-  }
-}
+  location.hash = '#start';
+  showQuestion('start');
 
-document.body.onsubmit = event => {
-  event.preventDefault();
-  const selectedRadio = event.target.querySelector('input[name="answer"]:checked');
-  if (selectedRadio) {
-    goToSection(selectedRadio.value);
+  window.onhashchange = () => {
+    if (location.hash.length > 1) {
+      showQuestion(location.hash.slice(1));
+    }
+    document.body.classList.toggle('can-go-back', location.hash && location.hash !== '#start');
   }
-}
+
+  function showQuestion(questionId) {
+    const question = questions[questionId];
+    if (!question) {
+      return;
+    }
+
+    document.querySelector('#title').textContent = question.title;
+    document.querySelector('#description').textContent = question.description;
+
+    const answers = document.getElementById('answers');
+    answers.innerHTML = question.answers.map(answer => `
+      <label>
+        <input type="radio" name="answer">
+        <span>
+          <div class="text"></div>
+          <div class="detail"></div>
+        </span>
+      </label>`
+    ).join('');
+    for (let i=0; i<question.answers.length; i++) {
+      answers.children[i].querySelector('input').value = question.answers[i].id;
+      answers.children[i].querySelector('.text').textContent = question.answers[i].text;
+      answers.children[i].querySelector('.detail').textContent = question.answers[i].detail;
+    }
+  }
+
+  document.querySelector('form').onsubmit = event => {
+    event.preventDefault();
+    const selectedRadio = event.target.querySelector('input[name="answer"]:checked');
+    if (selectedRadio) {
+      location.hash = '#' + selectedRadio.value;
+    }
+  }
+
+  document.getElementById('back-button').onclick = () => history.back();
+})();
